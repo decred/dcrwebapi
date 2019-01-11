bcd3ba5e8d98e6646a6b7d408823eecd7f868637
Jason Wong <jason@coinfund.io>
2019-01-10 22:13:44 -0500

    added the new pool entry for https://dcr.grassfed.network

--- service.go
+++ service.go
@@ -157,6 +157,12 @@ func NewService() *Service {
 		//   - https://github.com/decred/dcrwebapi/commit/9374b388624ad2b3f587d3effef39fc752d892ec
 		//   - https://github.com/decred/dcrwebapi/commit/e76f621d33050a506ab733ff2bc2f47f9366726c
 		Stakepools: StakepoolSet{
+			"Grassfed": {
+				APIVersionsSupported: []interface{}{},
+				Network:              "mainnet",
+				URL:                  "https://dcr.grassfed.network",
+				Launched:             getUnixTime(2019, 1, 10, 03, 27),
+			},
 			"Dittrex": {
 				APIVersionsSupported: []interface{}{},
 				Network:              "mainnet",
