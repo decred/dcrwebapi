b6656e04bf20b9cf7d1796738a52a7d0420fc9a2
David00 <github@dalbrecht.tech>
2021-02-02 07:28:18 -0800

    Add DecredVoting to vspd list (#127)

--- service.go
+++ service.go
@@ -222,6 +222,10 @@ func NewService() *Service {
 				Network:  "mainnet",
 				Launched: getUnixTime(2020, 12, 9),
 			},
+			"decredvoting.com": Vsp{
+				Network:  "mainnet",
+				Launched: getUnixTime(2021, 2, 1),
+			},
 		},
 
 		// Historical launch dates have been collected from these sources:
