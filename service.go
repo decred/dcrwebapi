15b27784db3cc8c06c3e195ace92bfb63e8f6ab9
feeleep75 <mfpass@gmail.com>
2021-02-10 17:18:19 +0100

    Add vsp.coinmine.pl to VSP LIST (#128)

--- service.go
+++ service.go
@@ -230,6 +230,10 @@ func NewService() *Service {
 				Network:  "mainnet",
 				Launched: getUnixTime(2021, 2, 1),
 			},
+			"vsp.coinmine.pl": Vsp{
+				Network:  "mainnet",
+				Launched: getUnixTime(2021, 1, 28),
+			},
 		},
 
 		// Historical launch dates have been collected from these sources:
